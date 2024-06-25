e0d5de3c0ba067924e386c2be38194b159416070
SaadatMitalipova <155878243+SaadatMitalipova@users.noreply.github.com>
2024-06-25 15:59:45 +0600

    Time: 64 ms (19.27%), Space: 50.8 MB (64.01%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -4,19 +4,5 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    let left = 0;
-    let right = letters.length - 1;
-    if (target >= letters[right]) {
-        return letters[0];
-    }
-    while (left < right) {
-        const mid = Math.floor((left + right) / 2);
-        if (letters[mid] > target) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
-    }
-    return letters[left];
-    
-};
+    return letters.find(c => c > target) || letters[0];
+}
