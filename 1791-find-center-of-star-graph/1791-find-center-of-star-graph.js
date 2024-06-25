3b7da2b7f57dab3e2eef5c5895064dc9300e5bd3
SaadatMitalipova <155878243+SaadatMitalipova@users.noreply.github.com>
2024-06-25 13:16:51 +0600

    Time: 88 ms (54.04%), Space: 64.5 MB (55.43%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+ let findCenter = function(edges) {
+    const [u1, v1] = edges[0];
+    const [u2, v2] = edges[1];
+        if (u1 === u2 || u1 === v2) {
+        return u1;
+    } else {
+        return v1;
+    }
+    
+};
+
