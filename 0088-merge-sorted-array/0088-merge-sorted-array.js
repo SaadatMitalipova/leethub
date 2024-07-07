3b0626ef393f7b732189806d240de232824aab6f
Saadat <155878243+SaadatMitalipova@users.noreply.github.com>
2024-07-07 11:04:39 +0600

    Time: 52 ms (65.17%), Space: 49.1 MB (41.24%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,10 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 
-
 var merge = function(nums1, m, nums2, n) {
-    for (let i= m, j=0; j<n; i++) {
+    for (let i = m, j = 0; j < n; i++, j++) {
         nums1[i] = nums2[j];
     }
-    nums1.sort((m, n) => m - n);
+    nums1.sort((a, b) => a - b);
 };
